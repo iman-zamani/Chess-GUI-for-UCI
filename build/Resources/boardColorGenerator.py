8b0d7144de55ab84cfd181d2f2b2c2f8f4087c23
imzzzzzz <izamanimoghaddam@gmail.com>
2024-08-22 12:49:32 +0330

    new board color

--- build/Resources/boardColorGenerator.py
+++ build/Resources/boardColorGenerator.py
@@ -5,9 +5,12 @@ from PIL import Image, ImageDraw
 img = Image.new('RGBA', (480, 240), (255, 255, 255, 255))
 draw = ImageDraw.Draw(img)
 
-
-color1 = (239, 210, 185, 255)  # first rectangle color
-color2 = (158, 110, 89, 255)   # second rectangle color
+#brownish 
+#color1 = (239, 210, 185, 255)  # first rectangle color
+#color2 = (158, 110, 89, 255)   # second rectangle color
+#blueish
+color1 = (134, 142, 150, 255)  # first rectangle color
+color2 = (233, 236, 239, 255)   # second rectangle color
 
 # draw rectangles
 draw.rectangle([0, 0, 240, 240], fill=color1)
