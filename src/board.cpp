a8691cb6df01db77c84198f4c747e4770230518c
iman zamani <izamanimoghaddam@gmail.com>
2025-12-13 17:45:24 +0330

    Declared GPLv2 in Every Source File

--- src/board.cpp
+++ src/board.cpp
@@ -1,4 +1,21 @@
-#include <iostream>
+/*
+ * Chess-GUI-for-UCI
+ * Copyright (C) 2025 Iman Zamani
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <https://www.gnu.org/licenses/>.
+ */
+
+ #include <iostream>
 #include <SFML/Graphics.hpp>
 #include<vector>
 #include<string>
--- src/values.hpp
+++ src/values.hpp
@@ -1,3 +1,20 @@
+/*
+ * Chess-GUI-for-UCI
+ * Copyright (C) 2025 Iman Zamani Moghaddam
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <https://www.gnu.org/licenses/>.
+ */
+
 #pragma once 
 
 #define BLACK_PAWN -1
